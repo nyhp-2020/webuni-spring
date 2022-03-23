060855ed6a08f9fe2684533899e66987bf82b96b
nyhp2020 <nyhp@freemail.hu>
2022-03-23 21:12:42 +0100

    getEmployee,modifyEmployee

--- hr/src/main/java/hu/webuni/hr/nyhp/web/HrTLController.java
+++ hr/src/main/java/hu/webuni/hr/nyhp/web/HrTLController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,49 +14,50 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import hu.webuni.hr.nyhp.model.Employee;
 
-
-
 @Controller
 public class HrTLController {
 
 	private List<Employee> allEmployees = new ArrayList<>();
-	private Map<Integer, Integer> indexEmployees = new TreeMap<>();
 
 	{
-		
-		//LocalDateTime ldt = LocalDateTime.of(2019, 9, 14, 0, 0, 0, 0);
+
+		// LocalDateTime ldt = LocalDateTime.of(2019, 9, 14, 0, 0, 0, 0);
 		Employee e1 = new Employee(333, "Peter", "Manager", 10000, LocalDateTime.of(2019, 9, 14, 0, 0, 0, 0));
 		allEmployees.add(e1);
-		//allEmployees.add(new Employee());
+		// allEmployees.add(new Employee());
 	}
-	
+
 	@GetMapping("/")
 	public String home() {
 		return "index";
 	}
-	
+
 	@GetMapping("/employees")
-	public String listEmployees(Map<String,Object> model) {
+	public String listEmployees(Map<String, Object> model) {
 		model.put("employees", allEmployees);
-		model.put("newEmployee", new Employee(0,"","",0,LocalDateTime.now()));
+		model.put("newEmployee", new Employee(0, "", "", 0, LocalDateTime.now()));
 		return "employees";
 	}
-	
+
 	@PostMapping("/employees")
 	public String addEmployee(Employee employee) {
 		allEmployees.add(employee);
 		return "redirect:employees";
 	}
-	
+
 	@GetMapping("/employee/{id}")
-	public String getEmployee(Map<String,Object> model,@PathVariable int id) {
-		System.out.println(id);
-		System.out.println(allEmployees.get(0));
-		
-		
-		model.put("employees", allEmployees);
-		model.put("oldEmployee", allEmployees.get(0));
+	public String getEmployee(Map<String, Object> model, @PathVariable int id) {
+		Employee em = allEmployees.stream().filter(e -> e.getId() == id).findAny().get();
+		model.put("oldEmployee", em);
 		return "employee";
 	}
-	
+
+	@PostMapping("/employee")
+	public String modifyEmployee(Employee employee) {
+		Employee em = allEmployees.stream().filter(e -> e.getId() == employee.getId()).findAny().get();
+		int index = allEmployees.indexOf(em);
+		allEmployees.set(index, employee);
+		return "redirect:employees";
+	}
+
 }
