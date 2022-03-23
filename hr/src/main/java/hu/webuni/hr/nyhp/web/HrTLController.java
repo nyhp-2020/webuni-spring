476617e55da784d270d4965fb8bee6bc3b5f0705
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-03-23 16:23:32 +0100

    employee.html

--- hr/src/main/java/hu/webuni/hr/nyhp/web/HrTLController.java
+++ hr/src/main/java/hu/webuni/hr/nyhp/web/HrTLController.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import hu.webuni.hr.nyhp.model.Employee;
@@ -17,11 +19,12 @@ import hu.webuni.hr.nyhp.model.Employee;
 public class HrTLController {
 
 	private List<Employee> allEmployees = new ArrayList<>();
+	private Map<Integer, Integer> indexEmployees = new TreeMap<>();
 
 	{
 		
 		//LocalDateTime ldt = LocalDateTime.of(2019, 9, 14, 0, 0, 0, 0);
-		Employee e1 = new Employee(1, "Peter", "Manager", 10000, LocalDateTime.of(2019, 9, 14, 0, 0, 0, 0));
+		Employee e1 = new Employee(333, "Peter", "Manager", 10000, LocalDateTime.of(2019, 9, 14, 0, 0, 0, 0));
 		allEmployees.add(e1);
 		//allEmployees.add(new Employee());
 	}
@@ -43,4 +46,16 @@ public class HrTLController {
 		allEmployees.add(employee);
 		return "redirect:employees";
 	}
+	
+	@GetMapping("/employee/{id}")
+	public String getEmployee(Map<String,Object> model,@PathVariable int id) {
+		System.out.println(id);
+		System.out.println(allEmployees.get(0));
+		
+		
+		model.put("employees", allEmployees);
+		model.put("oldEmployee", allEmployees.get(0));
+		return "employee";
+	}
+	
 }
