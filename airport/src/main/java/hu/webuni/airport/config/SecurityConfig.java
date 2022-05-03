eb1182ea0a1269b4705b666355ecd26f71d607bd
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-05-03 16:09:10 +0200

    SecurityConfig

---
+++ airport/src/main/java/hu/webuni/airport/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package hu.webuni.airport.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.passwordEncoder(passwordEncoder())
+			.withUser("user").authorities("user").password(passwordEncoder().encode("pass"))
+			.and()
+			.withUser("admin").authorities("user","admin").password(passwordEncoder().encode("pass"));
+			
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.httpBasic()
+		.and()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/api/airports/**").hasAuthority("admin")
+		.antMatchers(HttpMethod.PUT, "/api/airports/**").hasAnyAuthority("user","admin")
+		.anyRequest().authenticated();
+	}
+	
+	
+
+}
