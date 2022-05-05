656f3a109168b220a8a028cc5665aab9c3bf587d
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-05-05 09:13:54 +0200

    HolidayMapper+

--- hr/src/main/java/hu/webuni/hr/nyhp/mapper/HolidayMapper.java
+++ hr/src/main/java/hu/webuni/hr/nyhp/mapper/HolidayMapper.java
@@ -1,12 +1,21 @@
 package hu.webuni.hr.nyhp.mapper;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import hu.webuni.hr.nyhp.dto.HolidayDto;
 import hu.webuni.hr.nyhp.model.Holiday;
 
 @Mapper(componentModel = "spring")
 public interface HolidayMapper {
+	
+	@Mapping(target = "claimer.pos", ignore = true)
+	@Mapping(target = "claimer.company", ignore = true)
+	@Mapping(target = "approver.pos", ignore = true)
+	@Mapping(target = "approver.company", ignore = true)
 	HolidayDto holidayToDto(Holiday holiday);
+	
+	@InheritInverseConfiguration
 	Holiday dtoToHoliday(HolidayDto holidayDto);
 }
