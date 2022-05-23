f063c29a43e6278ac3c28473c792329959141b70
nyhp2020 <nyhp@freemail.hu>
2022-05-23 22:34:46 +0200

    new project

---
+++ transport/src/main/java/hu/webuni/transport/nyhp/TransportApplication.java
@@ -0,0 +1,13 @@
+package hu.webuni.transport.nyhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportApplication.class, args);
+	}
+
+}
---
+++ transport/src/test/java/hu/webuni/transport/nyhp/TransportApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webuni.transport.nyhp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
