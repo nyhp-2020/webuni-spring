ab2468f6db11811acc3aa4b29c38a5a459857e29
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-05-25 10:37:51 +0200

    RequestBody

--- hr/src/main/java/hu/webuni/hr/nyhp/security/JwtLoginController.java
+++ hr/src/main/java/hu/webuni/hr/nyhp/security/JwtLoginController.java
@@ -6,10 +6,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import hu.webuni.hr.nyhp.dto.LoginDto;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class JwtLoginController {
