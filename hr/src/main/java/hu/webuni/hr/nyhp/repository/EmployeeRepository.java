51087bf96e9d8b45a43042d6e7c4a8d0c52d8ee4
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-05-25 11:13:39 +0200

    @EntityGraph

--- hr/src/main/java/hu/webuni/hr/nyhp/repository/EmployeeRepository.java
+++ hr/src/main/java/hu/webuni/hr/nyhp/repository/EmployeeRepository.java
@@ -41,6 +41,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSp
 	List<Object[]> getAvgSalaryOfCompanyGroupedByPosition(long id);
 
 	//@EntityGraph(attributePaths = { "pos", "company" })
-	@EntityGraph(attributePaths = { "managedEmployees","superior" })
+	@EntityGraph(attributePaths = { "managedEmployees","superior","roles" })
 	Optional<Employee> findByUsername(String username);
 }
